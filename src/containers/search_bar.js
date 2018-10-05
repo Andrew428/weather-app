171e610de5cd5e87712e090e6658cd9a63d4993e
Andrew VanCleave <avancleave@onlinecommunityservices.com>
2018-10-05 11:22:19 -0500

    Update search_bar.js

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,19 +2,34 @@ import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
 
-    render() {
-        return(
-            <form className="input-group">
-                <input />
-            
-            </form>
+    constructor(props){
+        super(props)
 
-
-        );
-
+        this.state = { term : '' }
+        this.onInputChange = this.onInputChange.bind(this);
 
     }
 
+    onInputChange(event) {
+        //console.log(event.target.value);
+        this.setState({ term : event.target.value });
+    }
 
+    render() {
+        return(
+            <form className="input-group">
+                <input 
+                    placeholder = "Get a 5 day forcast for you favorit cities"
+                    className = "form-control"
+                    value = { this.state.term }
+                    onChange = { this.onInputChange }
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            
+            </form>
+        );
+    }
 
 }
