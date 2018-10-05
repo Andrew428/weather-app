bed9093ff7091d0994daa3c9fe21b13ae51c7c26
Andrew VanCleave <avancleave@onlinecommunityservices.com>
2018-10-05 10:06:36 -0500

    Update Dependencies

---
+++ src/containers/search_bar.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+
+    render() {
+        return(
+            <form className="input-group">
+                <input />
+            
+            </form>
+
+
+        );
+
+
+    }
+
+
+
+}
